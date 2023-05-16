e4aeac76809f76bd6c4281e6034d9cac107be306
Daniel Osorio <d.osorioc2@uniandes.edu.co>
2023-05-16 09:29:40 -0500

    Internacionalizacion

--- src/components/job.js
+++ src/components/job.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {FormattedDate, FormattedMessage, FormattedNumber} from 'react-intl';
 
 const Job = (props) => {
   return (
@@ -6,11 +7,35 @@ const Job = (props) => {
       <th scope="row">{props.offer.id}</th>
       <td>{props.offer.name}</td>
       <td>{props.offer.company}</td>
-      <td>{props.offer.salary}</td>
+      <td>
+        {props.offer.salary} <FormattedMessage id={MillionPlural(props.offer.salary)}/>
+      </td>
       <td>{props.offer.city}</td>
-      <td>{props.offer.date}</td>
+      <td>
+        <FormattedDate
+          value={new Date(props.offer.date)}
+          year='numeric'
+          month='long'
+          day='numeric'
+          weekday='long'
+        />
+      </td>
+      <td>
+        <FormattedNumber
+          value={props.offer.views}
+        />
+      </td>
     </tr>
   );
 };
+
+function MillionPlural(value) {
+  if (value === 1) {
+    return "Million";
+  }
+  else {
+    return "Millions";
+  }
+}
 
 export default Job;
